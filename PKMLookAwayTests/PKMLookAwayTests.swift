d24783ef7b2f8b969459fbd67dbd3b6656df0d3f
Patrick Kobler <patrick.kobler@gmail.com>
2024-12-31 10:35:10 +0100

    Initial Commit

---
+++ PKMLookAwayTests/PKMLookAwayTests.swift
@@ -0,0 +1,16 @@
+//
+//  PKMLookAwayTests.swift
+//  PKMLookAwayTests
+//
+//  Created by Patrick Kobler on 31.12.2024.
+//
+
+import Testing
+
+struct PKMLookAwayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
